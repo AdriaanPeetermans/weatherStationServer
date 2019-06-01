36dbfd15e5cc9362060eef0b75cbec0dd5dfac1a
AdriaanPeetermans <adriaanpeetermans@live.be>
2019-06-01 10:48:53 +0200

    Added data.

--- WeatherStation/src/testers/BasisHandler_tb.java
+++ WeatherStation/src/testers/BasisHandler_tb.java
@@ -7,16 +7,16 @@ public class BasisHandler_tb {
 
 	public static void main(String[] args) {
 		//BasisHandler bh = new BasisHandler("192.168.1.61", 9876, 10, 5, 1);
-		BasisHandler bh = new BasisHandler("127.0.0.1", 9875, 10, 5, 1);
+		BasisHandler bh = new BasisHandler("192.168.1.61", 9876, 10, 5, 1);
 		//ForecastParser parser = new ForecastParser();
 		//WeatherData[] data = parser.parseFile();
 		//System.out.println(bh.createTextForecast(data[0]));
 		try {
 			//bh.setTime();
 			//System.out.println(bh.getDayIndex());
-			//bh.transferFiles(6);
+			bh.transferFiles(6);
 			//bh.setDayCounter(8);
-			System.out.println(bh.getBasisToPiData().toString());
+			//System.out.println(bh.getBasisToPiData().toString());
 		} catch (BasisException e) {
 			e.printStackTrace();
 		}
