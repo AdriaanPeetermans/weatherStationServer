3c0cc3881abe83c52b34fbd7bf033bacf53c0150
AdriaanPeetermans <adriaanpeetermans@live.be>
2019-09-29 19:36:20 +0200

    Added data.

--- WeatherStation/src/testers/BasisHandler_tb.java
+++ WeatherStation/src/testers/BasisHandler_tb.java
@@ -6,7 +6,7 @@ import exceptions.BasisException;
 public class BasisHandler_tb {
 
 	public static void main(String[] args) {
-		BasisHandler bh = new BasisHandler("192.168.1.58", 9876, 10, 5, 1);
+		BasisHandler bh = new BasisHandler("192.168.1.60", 9876, 10, 5, 1);
 		//BasisHandler bh = new BasisHandler("127.0.0.1", 9876, 10, 5, 1);
 		//ForecastParser parser = new ForecastParser();
 		//WeatherData[] data = parser.parseFile();
